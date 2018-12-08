149cc80f644e0d2f7aedaabb39cb2d89d3e16d62
Payden Boughton <paydendanielboughton@ymail.com>
2018-12-07 20:13:19 -0500

    merged with DBConnector

--- src/Business/DBConnector.java
+++ src/Business/DBConnector.java
@@ -7,27 +7,40 @@ package Business;
 public class DBConnector {
 
 //EDUPE
+    private final String LOGIN = "3934";
+    private final String PASSWORD = "paydenboughton";
+    private final String URL = "jdbc:mysql://devry.edupe.net:4300/cis355a_3934";
+    private final String STATEMENT = "SELECT CustomerName,CustomerAddress,FlooringType,FloorArea,FloorCost FROM flooring";
+    private final String INSERT = "insert into flooring (CustomerName, CustomerAddress, FlooringType, FloorArea, FloorCost) values(?,?,?,?,?)";
 
-private final String login = "3934";
-private final String password = "paydenboughton";
-private final String url = "jdbc:mysql://devry.edupe.net:4300/cis355a_3934";
     /*
  //my local database
 private final String login = "root";
 private final String password = "mylove922";
 private final String url = "jdbc:mysql://localhost:3306/cis355a";
- */
+     */
 
-    public DBConnector(){}
-    
-    public String getlogin(){
-        return login;
+    public DBConnector() {
     }
-    public String getpassword(){
-        return password;
+
+    public String getlogin() {
+        return LOGIN;
     }
-    public String geturl(){
-        return url;
+
+    public String getpassword() {
+        return PASSWORD;
     }
-    
+
+    public String geturl() {
+        return URL;
+    }
+
+    public String statementQuery() {
+        return STATEMENT;
+    }
+
+    public String insertQuery() {
+        return INSERT;
+    }
+
 }
